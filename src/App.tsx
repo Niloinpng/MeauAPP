f787e5bf237f452b5cc6035c48a012275dc0ceeb
Nilo Silveira <danilo.silveira@cast.com.br>
2025-09-14 17:17:51 -0300

    :sparkles: Telas estaticas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { Assets as NavigationAssets } from '@react-navigation/elements';
 import { DarkTheme, DefaultTheme } from '@react-navigation/native';
 import { Asset } from 'expo-asset';
+import { useFonts } from 'expo-font';
+import { useEffect, useState } from 'react';
 import { createURL } from 'expo-linking';
+import { View, StyleSheet, Image } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import * as React from 'react';
 import { useColorScheme } from 'react-native';
@@ -20,6 +23,41 @@ const prefix = createURL('/');
 export function App() {
   const colorScheme = useColorScheme();
 
+  const [loaded] = useFonts({
+    'SpaceMono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+    'Courgette-Regular': require('../assets/fonts/Courgette-Regular.ttf'), 
+    'Roboto-Regular': require('../assets/fonts/Roboto-Regular.ttf'),    
+  });
+
+    const [isSplashVisible, setSplashVisible] = useState(true);
+
+  useEffect(() => {
+    if (loaded) {
+      setTimeout(() => {
+        setSplashVisible(false);
+      }, 3000); 
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  if (isSplashVisible) {
+    return (
+      <View style={styles.splashContainer}>
+        <Image 
+          source={require('./assets/images/Meau_malha.png')} 
+          style={styles.backgroundImage} 
+        />
+        <Image 
+          source={require('./assets/images/Meau_marca.png')} 
+          style={styles.logoImage} 
+        />
+      </View>
+    );
+  }
+
   const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme
 
   return (
@@ -35,3 +73,23 @@ export function App() {
     />
   );
 }
+
+const styles = StyleSheet.create({
+  splashContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#88c9bf',
+  },
+  backgroundImage: {
+    position: 'absolute', 
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover', 
+  },
+  logoImage: {
+
+    width: 300, 
+    resizeMode: 'contain',
+  },
+});
--- src/navigation/screens/Introducao.tsx
+++ src/navigation/screens/Introducao.tsx
@@ -1,25 +1,56 @@
-import { Button, Text } from '@react-navigation/elements';
-import { StyleSheet, View } from 'react-native';
+import { Button } from '@react-navigation/elements';
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Image } from 'react-native';
 
 export function Introducao() {
   return (
     <View style={styles.container}>
-      <Text>Olá</Text>
-      <Text>Open up 'src/App.tsx' to start working on your app!</Text>
 
-      <Button screen="Adotar">
-        Adotar
-      </Button>
+      <View style={styles.content}>
 
-      <Button>Ajudar</Button>
+        <Text style={styles.title}>Olá!</Text>
 
-      <Button screen="CadastroAnimal">
-        Cadastrar Animal
-      </Button>
+        <Text style={styles.subtitle}>
+          Bem vindo ao Meau!{'\n'}
+          Aqui você pode adotar, doar e ajudar{'\n'}
+          cães e gatos com facilidade.{'\n'}
+          Qual o seu interesse?
+        </Text>
 
-      <Button screen="CadastroPessoal">
-        Login
-      </Button>
+        <Button 
+          style={styles.button}
+          screen="Adotar"
+        >
+          Adotar
+        </Button>
+
+        <Button 
+          style={styles.button}
+        >
+          Ajudar
+        </Button>
+
+        <Button           
+          style={styles.button}
+          screen="CadastroAnimal"
+        >
+          Cadastrar Animal
+        </Button>
+
+        <Button 
+          style={styles.loginText}
+          screen="CadastroPessoal"
+        >
+          Login
+        </Button>
+
+      </View>
+
+      <View style={styles.footer}>
+        <Image 
+          //source={require('../assets/images/Meau_marca_2.png')} 
+          style={styles.logoImage} 
+        />
+      </View>
 
     </View>
   );
@@ -28,8 +59,52 @@ export function Introducao() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footer: {
+    alignItems: 'center',
+    paddingBottom: 32, 
+  },
+  title: {
+    fontFamily: 'Courgette-Regular',
+    fontSize: 72,
+    color: '#ffd358',
+  },
+  subtitle: {
+    fontFamily: 'Roboto-Regular',
+    fontSize: 16,
+    color: '#757575',
+    textAlign: 'center',
+    marginTop: 32,
+    marginBottom: 32,
+    paddingHorizontal: 20, 
+  },
+  button: {
+    backgroundColor: '#ffd358',
+    width: 232,
+    height: 40,
+    borderRadius: 2,
     justifyContent: 'center',
     alignItems: 'center',
-    gap: 10,
+    marginBottom: 12,
+    fontFamily: 'Roboto-Regular',
+    fontSize: 12,
+    color: '#434343',
+  },
+  loginText: {
+    backgroundColor: 'transparent',
+    fontFamily: 'Roboto-Regular',
+    fontSize: 16,
+    color: '#88c9bf',
+    marginTop: 24,
+  },
+  logoImage: {
+    width: 120,
+    resizeMode: 'contain',
   },
 });
